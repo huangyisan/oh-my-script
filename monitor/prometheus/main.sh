9f9ab477393b140a0d654d8e3b1e6d06fbae58b6
Yisan Huang <anonymousyisan@gmail.com>
2024-10-16 20:57:10 +0800

    feat: source sub script

---
+++ monitor/prometheus/main.sh
@@ -0,0 +1,29 @@
+source ./prometheus_install.sh
+# 主菜单
+function main_menu() {
+    while true; do
+        clear
+        
+        echo "退出脚本，请按ctrl c退出即可"
+        echo "请选择要执行的操作:"`
+        echo "1. 安装prometheus最新release"
+        echo "2. 安装prometheus从本地文件"
+        # echo "2. 安装alertmanager"
+        # echo "3. 安装node_exporter"
+        read -p "请输入选项（1-3）: " OPTION
+
+        case $OPTION in
+        1) install_prometheus ;;
+        2) install_prometheus_local ;;
+        # 3) install_alertmanager ;;
+        # 4) install_node_exporter ;;
+        *) echo "无效选项。" ;;
+        esac
+        echo "按任意键返回主菜单..."
+        read -n 1
+    done
+
+}
+
+# 显示主菜单
+main_menu
--- monitor/prometheus/prometheus_install.sh
+++ monitor/prometheus/prometheus_install.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
-
-# 检查是否以root用户运行脚本
-if [ "$(id -u)" != "0" ]; then
-    echo "此脚本需要以root用户权限运行。"
-    echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
-    exit 1
-fi
-
-# 检查是否为 Ubuntu 系统
-cat /etc/os-release | grep -q "Ubuntu"
-if [ $? -ne 0 ]; then
-    echo "此脚本仅支持 Ubuntu 系统。"
-    exit 1
-fi
-
-# 检查是否为x64系统
-if [ "$(uname -m)" != "x86_64" ]; then
-    echo "此脚本仅支持x64系统。"
-    exit 1
-fi
+source ./pre_check.sh
 
 # prometheus env
 binary_path="/usr/local/sbin"
@@ -169,33 +150,3 @@ function install_prometheus_local() {
     _create_systemctl_config
     _start_prometheus  
 }
-
-
-# 主菜单
-function main_menu() {
-    while true; do
-        clear
-        
-        echo "退出脚本，请按ctrl c退出即可"
-        echo "请选择要执行的操作:"`
-        echo "1. 安装prometheus最新release"
-        echo "2. 安装prometheus从本地文件"
-        # echo "2. 安装alertmanager"
-        # echo "3. 安装node_exporter"
-        read -p "请输入选项（1-3）: " OPTION
-
-        case $OPTION in
-        1) install_prometheus ;;
-        2) install_prometheus_local ;;
-        # 3) install_alertmanager ;;
-        # 4) install_node_exporter ;;
-        *) echo "无效选项。" ;;
-        esac
-        echo "按任意键返回主菜单..."
-        read -n 1
-    done
-
-}
-
-# 显示主菜单
-main_menu
