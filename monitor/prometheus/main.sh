dd9ad667b44ccbb2760257f642ee5c267f81f993
Yisan Huang <anonymousyisan@gmail.com>
2024-10-17 11:23:32 +0800

    feat: update

--- monitor/prometheus/main.sh
+++ monitor/prometheus/main.sh
@@ -8,16 +8,16 @@ function main_menu() {
         echo "退出脚本，请按ctrl c退出即可"
         echo "请选择要执行的操作:"
         echo "1. 安装prometheus最新release"
-        echo "2. 安装prometheus从本地文件"
-        # echo "2. 安装alertmanager"
-        echo "3. 安装node_exporter"
-        read -p "请输入选项（1-3）: " OPTION
+        echo "2. 从本地安装prometheus"
+        echo "3. 安装node_exporter最新release"
+        echo "4. 从本地安装node_exporter"
+        read -p "请输入选项（1-9）: " OPTION
 
         case $OPTION in
         1) install_prometheus ;;
         2) install_prometheus_local ;;
-        # 3) install_alertmanager ;;
-        # 4) install_node_exporter ;;
+        3) install_node_exporter_local ;;
+        4) install_node_exporter ;;
         *) echo "无效选项。" ;;
         esac
         echo "按任意键返回主菜单..."
